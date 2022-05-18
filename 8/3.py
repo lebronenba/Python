5c3e5a9fa23a057cf859d38469bbd70b83e2695c
lebronenba <84011563+lebronenba@users.noreply.github.com>
2022-05-18 22:08:35 +0300

    Add files via upload

---
+++ 8/3.py
@@ -0,0 +1,5 @@
+dictionary_1 = {'a': 300, 'b': 400} #Создаем словарь
+dictionary_2 = {'c': 500, 'd': 600} #Создаем словарь
+dictionary_1.update(dictionary_2) #команда update соединяет 2 словаря в один
+print(dictionary_1) #выводим соедененный словарь
+print(dictionary_2) #выводим второй словарь
